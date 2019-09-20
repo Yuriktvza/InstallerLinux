5fbbffbe21e671ebca1d11e90c0ee5fd5ff2b945
Yuriktvza <j0rlfl7hcx1x@gmail.com>
2019-09-19 22:23:59 -0400

    Adding SHELLSCRIPT

---
+++ tiger.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+cat << "TIGER"
+  ("`-''-/").___..--''"`-._ 
+ `P_ P  )   `-.  (     ).`-.__.`) 
+ (_Y_.)'  ._   )  `._ `. ``-..-' 
+   _..`--'_..-_/  /--'_.'
+  ((((.-''  ((((.'  (((.-'   TIGER BASH.v0.0.1
+TIGER
+
+
+echo "Installing Git"
+sudo apt-get update
+sudo apt-get install git -y
+
+echo "Installing Ruby"
+sudo apt-get update
+sudo apt-get install ruby-full 
+
+echo "Installing Vim"
+sudo apt-get install vim -y
+
+echo "Installing OBS Studio"
+sudo apt-get install ffmpeg -y
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt-get update
+sudo apt-get install obs-studio -y
+
+echo "Installing Sublime-Text 3"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text 
+
+echo "##################################################"
+echo "##################################################"
+echo "################# FINISHED #######################"
+echo "##################################################"
+echo "##################################################"
