9e38f4081e2074493ed95cd4291d9a5345f5403d
Yuriktvza <j0rlfl7hcx1x@gmail.com>
2019-10-15 14:36:31 -0400

    adding

--- tiger.sh
+++ tiger.sh
@@ -16,6 +16,10 @@ echo "Installing Ruby"
 sudo apt-get update
 sudo apt-get install ruby-full 
 
+echo "Installing Java 8 JDK"
+sudo apt install openjdk-8-jdk -y
+java -version
+
 echo "Installing Vim"
 sudo apt-get install vim -y
 
